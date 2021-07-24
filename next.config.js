f11e59f7ac8989193693264474adb1bb3fba92f2
To-Ko5 <kobayashi.toshi.tk@gmail.com>
2021-07-24 22:59:14 +0900

    Add: fork-ts-checker-webpack-pluginを追加

--- next.config.js
+++ next.config.js
@@ -1,3 +1,19 @@
+const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin')
+
 module.exports = {
   reactStrictMode: true,
+  /**
+   * Custom Webpack Config
+   * https://nextjs.org/docs/api-reference/next.config.js/custom-webpack-config
+   */
+  webpack(config, options) {
+    const { dev, isServer } = options
+
+    // Do not run type checking twice:
+    if (dev && isServer) {
+      config.plugins.push(new ForkTsCheckerWebpackPlugin())
+    }
+
+    return config
+  }
 }
