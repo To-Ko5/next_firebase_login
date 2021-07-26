31fe2bb9f96e7dcef0f4546d87d98f063c930262
To-Ko5 <kobayashi.toshi.tk@gmail.com>
2021-07-26 09:17:26 +0900

    Add: ローディングのUIを追加

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,5 @@
+import loading from '../styles/loading.module.css'
+
 const Home = () => {
   const loginButtonClick = () => {
     console.log('login')
@@ -5,6 +7,9 @@ const Home = () => {
   return (
     <div className="flex justify-center items-center min-h-screen">
       <div>
+        <div
+          className={`${loading.loader} ease-linear rounded-full border-4 border-t-4 border-gray-200 h-12 w-12 mx-auto mb-4`}
+        ></div>
         <button
           onClick={loginButtonClick}
           className="px-8 py-3 border border-transparent text-base font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700"
